a437268b4bdedfc299dc4953a7d0b016e6aa094e
Invisiveuls <fonelglg@gmail.com>
2025-07-18 15:24:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify
-
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Servidor Flask Online!'
-
-@app.route('/toast', methods=['POST'])
-def toast():
-    msg = request.form.get('msg', 'Olá do servidor')
-    return jsonify({'toast_msg': msg})
+@app.route('/parimpar', methods=['POST'])
+def parimpar():
+    num = request.form.get('num')
+    try:
+        n = int(num)
+        res = "Par" if n % 2 == 0 else "Ímpar"
+    except:
+        res = "Número inválido"
+    return jsonify({'result': res})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
