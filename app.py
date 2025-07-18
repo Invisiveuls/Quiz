dba7cd5a11e106109673cefed57a1e400beb35dc
Invisiveuls <fonelglg@gmail.com>
2025-07-18 20:53:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-from flask import Flask, request, send_file
-import cv2
-import numpy as np
-import io
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/process_image_raw', methods=['POST'])
 def process_image_raw():
-    img_bytes = request.data
-    nparr = np.frombuffer(img_bytes, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, buf = cv2.imencode('.png', gray)
-    return send_file(io.BytesIO(buf.tobytes()), mimetype='image/png')
+    print("Requisição recebida!")  # Para ver no console
+    return "OK"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
