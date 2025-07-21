8ae2ac2bd239466a0c3d91533b0175d0cd476401
Invisiveuls <fonelglg@gmail.com>
2025-07-20 22:44:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import cv2
 import numpy as np
 import base64
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def detect():
         return jsonify({"erro": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
